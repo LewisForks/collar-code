deaa059687e4a7e66d440dc840a26783c7fb51e8
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2023-12-30 22:04:44 +0000

    Create dbHelper util

---
+++ src/utilities/dbHelper.js
@@ -0,0 +1,93 @@
+const { connect } = require('mongoose');
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+async function checkUserExists(connection, email) {
+    try {
+        const [rows, fields] = await connection.query('SELECT * FROM users WHERE email = ?', [email]);
+        return rows.length > 0;
+    } catch (error) {
+        console.error('Error checking user existence:', error);
+        throw error;
+    }
+}
+
+const checkVerified = async (connection, email) => {
+    try {
+        const [rows] = await connection.query(
+            'SELECT verified FROM users WHERE email = ?',
+            [email]
+        );
+        if (rows.length > 0) {
+            const isVerified = rows[0].verified === 1;
+            console.log('isverified: ', isVerified)
+            return isVerified;
+        } else {
+            // If no user found with the given email, consider them not verified
+            return false;
+        }
+    } catch (error) {
+        console.error('Error checking if user is verified:', error)
+    }
+};
+
+const getUserData = async (connection, email) => {
+    try {
+        const [rows] = await connection.query(
+            'SELECT * FROM users WHERE email = ?',
+            [email]
+        );
+
+        return rows.length > 0 ? rows[0] : null;
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        throw error;
+    }
+};
+
+const getHashedPassword = async (connection, email) => {
+    try {
+        const userData = await getUserData(connection, email);
+        return userData ? userData.password : null;
+    } catch (error) {
+        console.error('Error fetching password from user data:', error);
+        throw error;
+    }
+};
+
+const getVerificationData = async (connection, userId) => {
+    try {
+        const [rows] = await connection.query(
+            'SELECT * FROM user_verification WHERE user_id = ?',
+            [userId]
+        );
+
+        return rows.length > 0 ? rows[0] : null;
+    } catch (error) {
+        console.error('Error fetching verification data:', error);
+        throw error;
+    }
+};
+
+const hashString = async (string) => {
+    const saltRounds = 10;
+    return await bcrypt.hash(string, saltRounds);
+};
+
+module.exports = {
+    checkUserExists,
+    checkVerified,
+    getUserData,
+    getHashedPassword,
+    getVerificationData,
+    hashString,
+};
