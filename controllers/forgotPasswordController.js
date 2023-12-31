7e8c7465734ac9912fb4ba685aa2a230dec8b1f9
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2023-12-31 15:49:35 +0000

    Create forgotPasswordController

--- controllers/forgotPasswordController.js
+++ controllers/forgotPasswordController.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require('uuid');
 const nodemailer = require('nodemailer');
 const bcrypt = require('bcrypt');
 const mysql = require('mysql2/promise');
@@ -31,24 +32,136 @@ const pool = mysql.createPool({
     queueLimit: 0
 });
 
-function clearErrorMessages() {
-    const errorElements = document.querySelectorAll('.error-message');
-    errorElements.forEach((element) => {
-        element.textContent = '';
-    });
-}
+const saveResetTokenData = async (connection, userId, hashedToken, expirationTime) => {
+    await connection.query(
+        'INSERT INTO password_reset (user_id, token, created_at, expires_at) VALUES (?, ?, ?, ?)',
+        [userId, hashedToken, new Date(), new Date(Date.now() + expirationTime)]
+    );
+};
 
-// Helper function to display error messages
-function displayError(field, message) {
-    const errorElement = document.getElementById(`${field}Error`);
+const sendPasswordResetEmail = async (req, res) => {
+    let email = req.body.email;
+    console.log(email);
+    const connection = await pool.getConnection();
 
-    if (errorElement) {
-        if (message && /<[a-z][\s\S]*>/i.test(message)) {
-            errorElement.innerHTML = message;
-        } else {
+    try {
+        await connection.beginTransaction();
 
-            errorElement.textContent = message || '';
+        const currentUrl = process.env.APP_URL || "http://localhost:3000/";
+        const expirationTime = 6 * 60 * 60 * 1000; // 6 hr
+
+        const { user_id: _id } = await dbHelper.getUserId(connection, email)
+
+        const token = uuidv4() + _id;
+
+        const mailOptions = {
+            from: process.env.AUTH_EMAIL,
+            to: email,
+            subject: "Reset Your Password",
+            html: `<p>Click <a href="${currentUrl}forgot-password/${_id}/${token}">here</a> to reset your password. This link <b>expires in 6 hours</b>.</p>`,
+        };
+
+        const hashedToken = await dbHelper.hashString(token);
+
+        await saveResetTokenData(connection, _id, hashedToken, expirationTime);
+
+        await transporter.sendMail(mailOptions);
+
+        await connection.commit();
+
+        // generic success, dont tell user if it worked or not
+        res.json({
+            status: "PENDING",
+            message: "Password Reset Email Sent"
+        });
+    } catch (error) {
+        await connection.rollback();
+        console.error("Error during sendPasswordResetEmail:", error);
+
+        let errorMessage = "An error occurred during password reset email.";
+
+        if (error.message.includes("hashing")) {
+            errorMessage = "Failed to hash password reset data!";
+        } else if (error.message.includes("sending")) {
+            errorMessage = "Error sending password reset email.";
         }
+
+        res.json({
+            status: "FAILED",
+            errors: errorMessage
+        });
+    } finally {
+        connection.release();
     }
-}
+};
+
+const checkResetToken = async ({ _id, token }) => {
+    const connection = await pool.getConnection();
+    try {
+        await connection.beginTransaction();
+        const result = await dbHelper.getResetTokenData(connection, _id);
+        if (result) {
+            const { expires_at, token: storedToken } = result;
+
+            if (expires_at < Date.now()) {
+
+                try {
+                    const deleteResult = await connection.execute('DELETE FROM password_reset WHERE user_id = ?', [_id]);
+
+                    if (deleteResult && deleteResult[0] && deleteResult[0].affectedRows !== undefined) {
+                        console.log(`Deleted ${deleteResult[0].affectedRows} row(s)`);
+                    } else {
+                        console.error('Error deleting password reset:', deleteResult);
+                    }
+
+                    await connection.commit();
+
+                    return {
+                        status: "FAILED",
+                        errors: "Password reset link has expired. Please request a new one."
+                    };
+                } catch (error) {
+                    console.error('Error in DELETE operation:', error);
+                    await connection.rollback();
+                    return {
+                        status: "FAILED",
+                        error: "An error has occured. Try again."
+                    };
+                }
+            } else {
+                const match = await bcrypt.compare(token, storedToken);
+                console.log(match);
+
+                if (match) {
+                    return {
+                        status: "SUCCESS",
+                        message: "Valid Password Reset Token."
+                    };
+                } else {
+                    return {
+                        status: "FAILED",
+                        error: "Invalid Password Reset Token, check your inbox."
+                    };
+                }
+            }
+        } else {
+            return {
+                status: "FAILED",
+                error: "No password reset record found, please check your inbox."
+            };
+        }
+    } catch (error) {
+        console.error(error);
+        return {
+            status: "FAILED",
+            error: "An error has occured. Try again."
+        };
+    } finally {
+        connection.release();
+    }
+};
+
+module.exports = {
+    sendPasswordResetEmail,
+    checkResetToken,
 };
