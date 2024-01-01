17bd0c6be4b1527ea9c3752763abf4fdaaf4f2f6
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2024-01-01 14:18:45 +0000

    Add some more status handling

--- src/public/js/forgotpassword.js
+++ src/public/js/forgotpassword.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    document.getElementById('resetPasswordForm').addEventListener('submit', function (event) {
+    document.getElementById('forgotPasswordForm').addEventListener('submit', function (event) {
         event.preventDefault();
 
         const email = document.getElementById('email').value;
@@ -15,46 +15,46 @@ window.onload = function () {
         })
             .then((response) => response.json())
             .then((data) => {
+                var submittedContainer = document.querySelector('.submitted-container');
+                var forgotPasswordForm = document.querySelector('.form-container');
 
                 if (data.status === 'PENDING') {
-                    var submittedContainer = document.querySelector('.submitted-container');
-                    var forgotPasswordForm = document.querySelector('.form-container');
-
                     if (submittedContainer && forgotPasswordForm) {
                         submittedContainer.style.display = 'block';
                         forgotPasswordForm.style.display = 'none';
                     }
+                } else if (data.status === 'SUCCESS') {
+                    console.log('YAYYYYYY')
                 } else if (data.status === 'FAILED') {
                     if (submittedContainer && forgotPasswordForm) {
                         submittedContainer.style.display = 'block';
                         forgotPasswordForm.style.display = 'none';
                     }
                 } else {
-                    displayError('other', 'An unexpected error has occured.')
+                    displayError('other', 'An unexpected error has occurred.')
                 }
             })
             .catch((error) => {
                 console.error('Error during password reset:', error);
             });
     });
+};
 
-    function clearErrorMessages() {
-        const errorElements = document.querySelectorAll('.error-message');
-        errorElements.forEach((element) => {
-            element.textContent = '';
-        });
-    }
+function clearErrorMessages() {
+    const errorElements = document.querySelectorAll('.error-message');
+    errorElements.forEach((element) => {
+        element.textContent = '';
+    });
+}
 
-    function displayError(field, message) {
-        const errorElement = document.getElementById(`${field}Error`);
+function displayError(field, message) {
+    const errorElement = document.getElementById(`${field}Error`);
 
-        if (errorElement) {
-            if (message && /<[a-z][\s\S]*>/i.test(message)) {
-                errorElement.innerHTML = message;
-            } else {
-
-                errorElement.textContent = message || '';
-            }
+    if (errorElement) {
+        if (message && /<[a-z][\s\S]*>/i.test(message)) {
+            errorElement.innerHTML = message;
+        } else {
+            errorElement.textContent = message || '';
         }
     }
-};
+}
