b89e72e1b73a4c530ccebd979fd13ab5bbd9d4d3
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2024-01-06 16:49:06 +0000

    Change encryption & db connection method

--- src/controllers/user-portal/EmailVerification.controller.js
+++ src/controllers/user-portal/EmailVerification.controller.js
@@ -1,49 +1,39 @@
 const { v4: uuidv4 } = require('uuid');
-// const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 const bcrypt = require('bcrypt');
 const mysql = require('mysql2/promise')
-const dbHelper = require('../../utilities/data/User')
+const dbHelper = require('../../utilities/data/User');
+const { executeMysqlQuery } = require('../../utilities/mysqlHelper');
+const { decrypt } = require('../../utilities/aes');
 
 require('dotenv').config();
 
-// let transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//         user: process.env.AUTH_EMAIL,
-//         pass: process.env.AUTH_PASS,
-//     }
-// });
-
-// transporter.verify((error, success) => {
-//     if (error) {
-//         console.log(error);
-//     } else {
-//         console.log('Ready for emails');
-//     }
-// });
-
-const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.AUTH_EMAIL,
+        pass: process.env.AUTH_PASS,
+    }
 });
 
-const saveVerificationData = async (connection, userId, hashedUniqueString, expirationTime) => {
-    await connection.query(
+transporter.verify((error, success) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Ready for emails');
+    }
+});
+
+const saveVerificationData = async (userId, hashedUniqueString, expirationTime) => {
+    await executeMysqlQuery(
         'INSERT INTO user_verification (user_id, unique_string, created_at, expires_at) VALUES (?, ?, ?, ?)',
         [userId, hashedUniqueString, new Date(), new Date(Date.now() + expirationTime)]
     );
 };
 
 const sendVerificationEmail = async ({ _id, email }) => {
-    const connection = await pool.getConnection();
 
     try {
-        await connection.beginTransaction();
 
         const currentUrl = process.env.APP_URL || "http://localhost:3000/";
         const expirationTime = 6 * 60 * 60 * 1000; // 6 hr
@@ -54,16 +44,14 @@ const sendVerificationEmail = async ({ _id, email }) => {
             from: process.env.AUTH_EMAIL,
             to: email,
             subject: "Verify Your Email",
-            html: `<p>Verify your email address to complete the signup and login to your new account.</p><p>This link <b>expires in 6 hours</b>.</p><p>Click <a href="${currentUrl}verify/${_id}/${uniqueString}">here</a> to proceed.</p>`,
+            html: `<p>Verify your email address to complete the signup and login to your new account.</p><p>This link <b>expires in 6 hours</b>.</p><p>Click <a href="${currentUrl}account/verify/${_id}/${uniqueString}">here</a> to proceed.</p>`,
         };
 
         const hashedUniqueString = await dbHelper.hashString(uniqueString);
 
-        await saveVerificationData(connection, _id, hashedUniqueString, expirationTime);
+        await saveVerificationData(_id, hashedUniqueString, expirationTime);
 
         await transporter.sendMail(mailOptions);
-
-        await connection.commit();
 
         const response = {
             status: "PENDING",
@@ -72,7 +60,6 @@ const sendVerificationEmail = async ({ _id, email }) => {
 
         return response;
     } catch (error) {
-        await connection.rollback();
         console.error("Error during sendVerificationEmail:", error);
 
         let errorMessage = "An error occurred during email verification.";
@@ -89,46 +76,41 @@ const sendVerificationEmail = async ({ _id, email }) => {
         };
 
         throw response;
-    } finally {
-        connection.release();
     }
 };
 
 const checkVerification = async ({ userId, uniqueString }) => {
-    const connection = await pool.getConnection();
     try {
-        await connection.beginTransaction();
-        const result = await dbHelper.getVerificationData(connection, userId);
-
+        const result = await dbHelper.getVerificationData(userId);
+        const email = await dbHelper.getUserEmail(userId);
         if (result) {
 
             const { expires_at, unique_string } = result;
 
             if (expires_at < Date.now()) {
-                await connection.execute('DELETE FROM user_verification WHERE user_id = ?', [userId]);
-                await connection.execute('DELETE FROM users WHERE user_id = ?', [userId]);
+                await executeMysqlQuery('DELETE FROM user_verification WHERE user_id = ?', [userId]);
+                await executeMysqlQuery('DELETE FROM users WHERE user_id = ?', [userId]);
                 
                 return {
                     status: "FAILED",
                     message: "Link has expired. Please sign up again."
                 };
             } else {
-                const match = await bcrypt.compare(uniqueString, unique_string);
+                console.log('1:', uniqueString, '2:', unique_string)
+                const decryptedString = await decrypt(unique_string);
+                if (decryptedString === uniqueString) {
+                    await executeMysqlQuery('UPDATE users SET verified = 1 WHERE user_id = ?', [userId]);
+                    await executeMysqlQuery('DELETE FROM user_verification WHERE user_id = ?', [userId]);
 
-                if (match) {
-                    await connection.execute('UPDATE users SET verified = 1 WHERE user_id = ?', [userId]);
-                    await connection.execute('DELETE FROM user_verification WHERE user_id = ?', [userId]);
-                    
-                    await connection.commit()
                     return {
                         status: "SUCCESS",
-                        message: "Verification Successful.",
+                        message: "Verification Successful."
                     };
 
                 } else {
                     return {
                         status: "FAILED",
-                        message: "Invalid vereification details passed, please check your inbox."
+                        message: "Invalid verification details passed, please check your inbox."
                     };
                 }
             }
@@ -144,8 +126,6 @@ const checkVerification = async ({ userId, uniqueString }) => {
             status: "FAILED",
             message: "An error occured. Check console or try again."
         };
-    } finally {
-        connection.release();
     }
 };
 
