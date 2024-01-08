23c157878c91b362e8fdc377e2a3b0b5e10549ea
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2024-01-08 23:27:38 +0000

    Create savePet function

--- src/controllers/user-portal/PetProfile.controller.js
+++ src/controllers/user-portal/PetProfile.controller.js
@@ -5,3 +5,25 @@ const mysql = require('mysql2/promise');
 const dbHelper = require('../../utilities/data/User');
 const { executeMysqlQuery } = require('../../utilities/mysqlHelper');
 const { decrypt, encrypt } = require('../../utilities/aes');
+
+
+require('dotenv').config();
+
+async function savePet(userId, petId, petName, petBreed, petAge) {
+    try {
+        const result = await executeMysqlQuery(
+            'INSERT INTO pets (user_id, pet_id, petName, petBreed, petAge) VALUES (?, ?, ?, ?, ?)',
+            [userId, petId, petName, petBreed, petAge]
+        );
+
+        return {
+            status: "SUCCESS",
+            message: "Pet profile data saved.",
+            petId: petId
+        }
+    } catch (error) {
+        console.error('Error saving pet to database:', error);
+        throw error;
+    }
+}
+
