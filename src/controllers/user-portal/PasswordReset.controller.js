15bbb62ef3d90173e67979babbdc1994512f9892
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2024-01-08 19:41:13 +0000

    Change SQL exec method & fix routing/errors

--- src/controllers/user-portal/PasswordReset.controller.js
+++ src/controllers/user-portal/PasswordReset.controller.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2/promise');
 const dbHelper = require('../../utilities/data/User')
 const { validateResetPasswordInput } = require('./Validation.controller');
 const { decrypt } = require('../../utilities/aes');
+const { executeMysqlQuery } = require('../../utilities/mysqlHelper');
 
 require('dotenv').config();
 
@@ -23,18 +24,8 @@ transporter.verify((error, success) => {
     }
 });
 
-const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-
-const saveResetTokenData = async (connection, userId, hashedToken, expirationTime) => {
-    await connection.query(
+const saveResetTokenData = async (userId, hashedToken, expirationTime) => {
+    await executeMysqlQuery(
         'INSERT INTO password_reset (user_id, token, created_at, expires_at) VALUES (?, ?, ?, ?)',
         [userId, hashedToken, new Date(), new Date(Date.now() + expirationTime)]
     );
@@ -43,15 +34,37 @@ const saveResetTokenData = async (connection, userId, hashedToken, expirationTim
 const sendPasswordResetEmail = async (req, res) => {
     let email = req.body.email;
     console.log(email);
-    const connection = await pool.getConnection();
 
     try {
-        await connection.beginTransaction();
+        const { user_id: _id } = await dbHelper.getUserId(email);
+        const resetTokenData = await dbHelper.getResetTokenData(_id);
+        if (resetTokenData) {
+            const resetTokenCreatedAt = new Date(resetTokenData.created_at).getTime();
+            const currentTime = new Date().getTime();
+            const timeDifferenceInSeconds = (currentTime - resetTokenCreatedAt) / 1000;
+
+            if (timeDifferenceInSeconds >= 60) {
+                // been more than 60 seconds - delete old token to create new
+                console.log('more than 60 seconds');
+                const deleteResult = await executeMysqlQuery('DELETE FROM password_reset WHERE user_id = ?', [_id]);
+
+                    if (deleteResult && deleteResult[0] && deleteResult[0].affectedRows !== undefined) {
+                        console.log(`Deleted ${deleteResult[0].affectedRows} row(s)`);
+                    } else {
+                        console.error('Error deleting old password reset token:', deleteResult);
+                    }
+            } else {
+                console.log('not been more than 60 seconds');
+
+                return res.status(400).json({
+                    status: "FAILED",
+                    error: "Please wait 60 seconds before resending the password reset email."
+                });                
+            }
+        }
 
         const currentUrl = process.env.APP_URL || "http://localhost:3000/";
         const expirationTime = 6 * 60 * 60 * 1000; // 6 hr
-
-        const { user_id: _id } = await dbHelper.getUserId(connection, email)
 
         const token = uuidv4() + _id;
 
@@ -59,24 +72,21 @@ const sendPasswordResetEmail = async (req, res) => {
             from: process.env.AUTH_EMAIL,
             to: email,
             subject: "Reset Your Password",
-            html: `<p>Click <a href="${currentUrl}forgot-password/${_id}/${token}">here</a> to reset your password. This link <b>expires in 6 hours</b>.</p>`,
+            html: `<p>Click <a href="${currentUrl}account/forgot-password/${_id}/${token}">here</a> to reset your password. This link <b>expires in 6 hours</b>.</p>`,
         };
 
         const hashedToken = await dbHelper.hashString(token);
 
-        await saveResetTokenData(connection, _id, hashedToken, expirationTime);
+        await saveResetTokenData(_id, hashedToken, expirationTime);
 
         await transporter.sendMail(mailOptions);
 
-        await connection.commit();
-
         // generic success, dont tell user if it worked or not
-        res.json({
+        return res.status(200).json({
             status: "PENDING",
             message: "Password Reset Email Sent"
         });
     } catch (error) {
-        await connection.rollback();
         console.error("Error during sendPasswordResetEmail:", error);
 
         let errorMessage = "An error occurred during password reset email.";
@@ -87,20 +97,16 @@ const sendPasswordResetEmail = async (req, res) => {
             errorMessage = "Error sending password reset email.";
         }
 
-        res.json({
+        return res.status(400).json({
             status: "FAILED",
-            errors: errorMessage
+            error: errorMessage
         });
-    } finally {
-        connection.release();
     }
 };
 
 const checkResetToken = async ({ _id, token }) => {
-    const connection = await pool.getConnection();
     try {
-        await connection.beginTransaction();
-        const result = await dbHelper.getResetTokenData(connection, _id);
+        const result = await dbHelper.getResetTokenData(_id);
         console.log('result from here:', result);
         if (result) {
             const { expires_at, token: storedToken } = result;
@@ -108,7 +114,7 @@ const checkResetToken = async ({ _id, token }) => {
             if (expires_at < Date.now()) {
 
                 try {
-                    const deleteResult = await connection.execute('DELETE FROM password_reset WHERE user_id = ?', [_id]);
+                    const deleteResult = await executeMysqlQuery('DELETE FROM password_reset WHERE user_id = ?', [_id]);
 
                     if (deleteResult && deleteResult[0] && deleteResult[0].affectedRows !== undefined) {
                         console.log(`Deleted ${deleteResult[0].affectedRows} row(s)`);
@@ -116,15 +122,12 @@ const checkResetToken = async ({ _id, token }) => {
                         console.error('Error deleting password reset:', deleteResult);
                     }
 
-                    await connection.commit();
-
                     return {
                         status: "FAILED",
-                        errors: "Password reset link has expired. Please request a new one."
+                        error: "Password reset link has expired. Please request a new one."
                     };
                 } catch (error) {
                     console.error('Error in DELETE operation:', error);
-                    await connection.rollback();
                     return {
                         status: "FAILED",
                         error: "An error has occured. Try again."
@@ -134,7 +137,7 @@ const checkResetToken = async ({ _id, token }) => {
                 const decryptedToken = decrypt(storedToken);
                 console.log(decryptedToken === token);
 
-                if (match) {
+                if (decryptedToken === token) {
                     return {
                         status: "SUCCESS",
                         message: "Valid Password Reset Token."
@@ -158,8 +161,6 @@ const checkResetToken = async ({ _id, token }) => {
             status: "FAILED",
             error: "An error has occured. Try again."
         };
-    } finally {
-        connection.release();
     }
 };
 
@@ -167,11 +168,8 @@ const resetPassword = async (req, res) => {
     let password = req.body.password;
     let _id = req.params._id
     let token = req.params.token
-    const connection = await pool.getConnection();
 
     try {
-        await connection.beginTransaction();
-
         tokenResult = await checkResetToken({ _id, token });
 
         if (tokenResult.status === "SUCCESS") {
@@ -182,23 +180,20 @@ const resetPassword = async (req, res) => {
 
             const hashedPassword = await dbHelper.hashString(password);
 
-            const saveNewPassword = await dbHelper.changePassword(connection, _id, hashedPassword);
+            const saveNewPassword = await dbHelper.changePassword(_id, hashedPassword);
 
             if (saveNewPassword) {
                 res.json({
                     status: "SUCCESS",
                 });
-                await connection.execute('DELETE FROM password_reset WHERE user_id = ?', [_id]);
-                connection.commit();
+                await executeMysqlQuery('DELETE FROM password_reset WHERE user_id = ?', [_id]);
             } else {
                 res.json({
                     status: "FAILED",
-                    errors: {
+                    error: {
                         other: 'Failed to save new password, please try again.'
                     }
                 });
-
-                await connection.rollback();
             }
         }
 
@@ -208,8 +203,6 @@ const resetPassword = async (req, res) => {
             status: "FAILED",
             error: "An error has occured. Try again."
         };
-    } finally {
-        connection.release();
     }
 }
 
