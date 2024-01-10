07580da6153ba4cd6c416d0de381aaa998a1193c
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2024-01-10 09:18:42 +0000

    Add hamburger menu to root,js

--- src/public/js/root.js
+++ src/public/js/root.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById('menu-icon').addEventListener('click', function () {
-        var mobileNavbar = document.querySelector('.mobile-navbar');
-        mobileNavbar.classList.toggle('show');
-    });
+    document.getElementById('menu-icon').addEventListener('click', toggleMobileNavbar);
 });
+
+function toggleMobileNavbar(event) {
+    event.preventDefault();
+
+    var mobileNavbar = document.querySelector('.mobile-navbar');
+    mobileNavbar.classList.toggle('show');
+}
