675c60f72085a5cd1054553bbd42079512f8acdb
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2024-01-18 17:44:11 +0000

    add accountupdate validation to backend

--- src/controllers/user-portal/Validation.controller.js
+++ src/controllers/user-portal/Validation.controller.js
@@ -19,7 +19,46 @@ const validateSignupInput = (name, email, password, dateOfBirth) => {
     // Validate password
     if (!password || !validator.isLength(password, { min: 8, max: 32 })) {
         errors.password = "Password must be between 8 and 32 characters.";
-    }    
+    }
+
+    // Validate date of birth
+    if (!dateOfBirth || !validator.isDate(dateOfBirth, { format: 'YYYY-MM-DD', strictMode: false })) {
+        errors.dateOfBirth = "Valid date of birth (YYYY-MM-DD) is required.";
+    }
+
+    // Check if there are any validation errors
+    if (Object.keys(errors).length === 0) {
+        return null; // No errors, validation successful
+    }
+
+    // Return the validation errors
+    return {
+        status: "FAILED",
+        message: "Validation failed.",
+        errors,
+    };
+};
+
+const validateAccountUpdateInput = (name, email, password, dateOfBirth) => {
+
+    const errors = {};
+
+    // Validate name
+    if (!name || !validator.isLength(name, { min: 1 })) {
+        errors.name = "Name is required.";
+    } else if (!validator.isAlpha(name.replace(/\s+/g, ''))) {
+        errors.name = "Name can only contain letters and spaces.";
+    }
+
+    // Validate email
+    if (!email || !validator.isEmail(email)) {
+        errors.email = "Valid email is required.";
+    }
+
+    // Validate password
+    if (password && !validator.isLength(password, { min: 8, max: 32 })) {
+        errors.password = "Password must be between 8 and 32 characters.";
+    }
 
     // Validate date of birth
     if (!dateOfBirth || !validator.isDate(dateOfBirth, { format: 'YYYY-MM-DD', strictMode: false })) {
@@ -78,12 +117,12 @@ const validatePetProfileInput = (petName, petBreed, petAge) => {
     } else if (!validator.isAlpha(petBreed.replace(/\s+/g, ''))) {
         errors.petBreed = "Pet's breed can only contain letters and spaces.";
     }
-    
+
     // Validate pet age
     if (!petAge || !validator.isInt(petAge, { min: 0 })) {
         errors.petAge = "Pet's age is required and should be more than 0.";
     }
-    
+
 
     // Check if there are any validation errors
     if (Object.keys(errors).length === 0) {
@@ -100,6 +139,7 @@ const validatePetProfileInput = (petName, petBreed, petAge) => {
 
 module.exports = {
     validateSignupInput,
+    validateAccountUpdateInput,
     validateResetPasswordInput,
     validatePetProfileInput,
 };
