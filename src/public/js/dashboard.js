7f5ec03cb95c440eb68478a7234c9c787b573760
Lewis Raybould <124367113+LewisForks@users.noreply.github.com>
2024-01-25 15:34:29 +0000

    Testing animations / transitions

--- src/public/js/dashboard.js
+++ src/public/js/dashboard.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = function () {     
 
     const menuOpen = document.getElementById('menu-open');
     const menuClose = document.getElementById('menu-close');
